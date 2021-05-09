b3cddcfe7aef0fbda2c175779087858ee3113054
yushan <yushan@intervest.lk>
2021-05-09 13:27:57 +0530

    test: Check addContactButton is exist in App component render

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/Ant Design/i);
-  expect(linkElement).toBeInTheDocument();
+test("expects addContactButton to be in App component", () => {
+  const { queryByTestId } = render(<App />);
+  const addContactButton = queryByTestId("add-contact-button");
+  expect(addContactButton).toBeInTheDocument();
 });
