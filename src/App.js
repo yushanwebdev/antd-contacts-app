72ed9f330135404b460dfd5abf074eb4a6a0b318
yushan <yushan@intervest.lk>
2021-05-09 13:28:47 +0530

    feat: Add <AddDrawer /> to <App />

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "antd";
 import { PlusCircleFilled } from "@ant-design/icons";
+import AddDrawer from "./AddDrawer";
+
+import "./App.less";
 
 const App = () => {
+  const [showDrawer, setShowDrawer] = useState(false);
   return (
-    <Button
-      type="primary"
-      icon={<PlusCircleFilled />}
-      data-testid="add-contact-button"
-    >
-      Add
-    </Button>
+    <>
+      <Button
+        type="primary"
+        icon={<PlusCircleFilled />}
+        data-testid="add-contact-button"
+        onClick={() => setShowDrawer(true)}
+      >
+        Add
+      </Button>
+      <AddDrawer show={showDrawer} handleOnClose={() => setShowDrawer(false)} />
+    </>
   );
 };
 
