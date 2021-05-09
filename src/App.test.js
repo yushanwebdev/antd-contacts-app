35e20b8a300cdfc4c8f07acdb4221abf91cd16d2
yushan <yushan@intervest.lk>
2021-05-09 14:10:00 +0530

    test: Test <Drawer /> is exist after addContactButton click

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
 import App from "./App";
 
 test("expects addContactButton to be in App component", () => {
@@ -12,4 +12,14 @@ test("expects Drawer component not to be in App component on render", () => {
   const { queryByTestId } = render(<App />);
   const addContactDrawer = queryByTestId("add-contact-drawer");
   expect(addContactDrawer).not.toBeInTheDocument();
+});
+
+test("expects Drawer component to be in App component on click", () => {
+  const { queryByTestId } = render(<App />);
+
+  const addContactButton = queryByTestId("add-contact-button");
+  fireEvent.click(addContactButton);
+
+  const addContactDrawer = queryByTestId("add-contact-drawer");
+  expect(addContactDrawer).toBeInTheDocument();
 });
