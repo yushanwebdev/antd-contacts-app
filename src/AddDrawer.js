42f3a6f32fc5edc842da4df926a366a93b58e59a
yushan <yushan@intervest.lk>
2021-05-14 22:04:54 +0530

    feat: Update form layout to vertical in <AddDrawer /> & add a reset button to it

--- src/AddDrawer.js
+++ src/AddDrawer.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import { Button, Drawer, Form, Input } from "antd";
 
@@ -35,6 +35,7 @@ const AddDrawer = ({
         initialValues={initialValues}
         onFinish={handleOnFinish}
         onFinishFailed={handleOnFinishFailed}
+        layout="vertical"
       >
         <Form.Item
           label="First Name"
@@ -77,17 +78,23 @@ const AddDrawer = ({
 
         <Form.Item shouldUpdate>
           {() => (
-            <Button
-              type="primary"
-              htmlType="submit"
-              disabled={
-                !form.isFieldsTouched(true) ||
-                !!form.getFieldsError().filter(({ errors }) => errors.length)
-                  .length
-              }
-            >
-              Log in
-            </Button>
+            <Fragment>
+              <Button
+                style={{ marginRight: 20 }}
+                type="primary"
+                htmlType="submit"
+                disabled={
+                  !form.isFieldsTouched(true) ||
+                  !!form.getFieldsError().filter(({ errors }) => errors.length)
+                    .length
+                }
+              >
+                Log in
+              </Button>
+              <Button htmlType="button" onClick={() => form.resetFields()}>
+                Reset
+              </Button>
+            </Fragment>
           )}
         </Form.Item>
       </Form>
