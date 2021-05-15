0d5f2fad6264ab0f06e2f478a7b26a8bdb42b423
yushan <yushan@intervest.lk>
2021-05-15 13:07:47 +0530

    feat: Update the Layout in <App />

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React, { useState } from "react";
-import { Button, Layout, Table } from "antd";
-import { CodeSandboxCircleFilled, PlusCircleFilled } from "@ant-design/icons";
+import { Button, Layout, Menu, Table, Breadcrumb } from "antd";
+import {
+  DesktopOutlined,
+  FileOutlined,
+  PieChartOutlined,
+  PlusCircleFilled,
+  TeamOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
 import AddDrawer from "./AddDrawer";
 
 import "./App.less";
+
+const { Header, Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
 
 const App = () => {
   const [showDrawer, setShowDrawer] = useState(false);
   const [values, setValues] = useState([]);
   const [errorInfo, setErrorInfo] = useState({});
+  const [collapsed, setCollapsed] = useState(false);
 
   const columns = [
     {
@@ -43,26 +54,72 @@ const App = () => {
     setErrorInfo(errorInfo);
   };
 
+  const onCollapse = (isCollapsed) => {
+    setCollapsed(isCollapsed);
+  };
+
   return (
-    <>
-      <Button
-        type="primary"
-        icon={<PlusCircleFilled />}
-        data-testid="add-contact-button"
-        onClick={() => setShowDrawer(true)}
-      >
-        Add
-      </Button>
-      <Layout.Content>
-        <Table dataSource={values} columns={columns} />
-      </Layout.Content>
-      <AddDrawer
-        show={showDrawer}
-        handleOnClose={() => setShowDrawer(false)}
-        handleOnFinish={handleAddFormOnFinish}
-        handleOnFinishFailed={handleAddFormOnFinishFailed}
-      />
-    </>
+    <Layout style={{ minHeight: "100vh" }}>
+      <Sider collapsible collapsed={collapsed} onCollapse={onCollapse}>
+        <div className="logo" />
+        <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline">
+          <Menu.Item key="1" icon={<PieChartOutlined />}>
+            Option 1
+          </Menu.Item>
+          <Menu.Item key="2" icon={<DesktopOutlined />}>
+            Option 2
+          </Menu.Item>
+          <SubMenu key="sub1" icon={<UserOutlined />} title="User">
+            <Menu.Item key="3">Tom</Menu.Item>
+            <Menu.Item key="4">Bill</Menu.Item>
+            <Menu.Item key="5">Alex</Menu.Item>
+          </SubMenu>
+          <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
+            <Menu.Item key="6">Team 1</Menu.Item>
+            <Menu.Item key="8">Team 2</Menu.Item>
+          </SubMenu>
+          <Menu.Item key="9" icon={<FileOutlined />}>
+            Files
+          </Menu.Item>
+        </Menu>
+      </Sider>
+      <Layout className="site-layout">
+        <Header className="site-layout-background" style={{ padding: 0 }} />
+        <Content style={{ margin: "0 16px" }}>
+          <Breadcrumb style={{ margin: "16px 0" }}>
+            <Breadcrumb.Item>User</Breadcrumb.Item>
+            <Breadcrumb.Item>Bill</Breadcrumb.Item>
+          </Breadcrumb>
+          <div
+            className="site-layout-background"
+            style={{ padding: 24, minHeight: 360 }}
+          >
+            <>
+              <Button
+                type="primary"
+                icon={<PlusCircleFilled />}
+                data-testid="add-contact-button"
+                onClick={() => setShowDrawer(true)}
+              >
+                Add
+              </Button>
+              <Layout.Content>
+                <Table dataSource={values} columns={columns} />
+              </Layout.Content>
+              <AddDrawer
+                show={showDrawer}
+                handleOnClose={() => setShowDrawer(false)}
+                handleOnFinish={handleAddFormOnFinish}
+                handleOnFinishFailed={handleAddFormOnFinishFailed}
+              />
+            </>
+          </div>
+        </Content>
+        <Footer style={{ textAlign: "center" }}>
+          Ant Design ©2018 Created by Ant UED
+        </Footer>
+      </Layout>
+    </Layout>
   );
 };
 
