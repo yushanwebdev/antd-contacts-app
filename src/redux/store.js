acb65603c3ffd583c573facfd246891f6e080f6a
yushan <yushan@intervest.lk>
2021-05-16 19:00:21 +0530

    feat: Configure the thunk in the store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,10 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 import contactReducer from "./contacts/reducer";
+import thunk from "redux-thunk";
 
-const store = createStore(combineReducers(contactReducer));
+const store = createStore(
+  combineReducers(contactReducer),
+  applyMiddleware(thunk)
+);
 
 export default store;
