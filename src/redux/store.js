b91a42f2b0acda3b52c59134dc3ff0767e817d11
yushan <yushan@intervest.lk>
2021-05-18 20:36:39 +0530

    feat: Whitelist contacts in the store

--- src/redux/store.js
+++ src/redux/store.js
@@ -11,6 +11,7 @@ const rootReducer = combineReducers({
 const persistConfig = {
   key: "root",
   storage,
+  whitelist: ["contacts"],
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
