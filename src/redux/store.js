7ce6208fddd28a9ab013b29364dc08a43d15114b
yushan <yushan@intervest.lk>
2021-05-18 20:30:27 +0530

    feat: Connected the store with the persistor

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,19 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
 import contactReducer from "./contacts/reducer";
 import thunk from "redux-thunk";
 
-const store = createStore(
-  combineReducers({ contacts: contactReducer }),
-  applyMiddleware(thunk)
-);
+const rootReducer = combineReducers({
+  contacts: contactReducer,
+});
 
-export default store;
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = createStore(persistedReducer, applyMiddleware(thunk));
+export const persistor = persistStore(store);
